eba5adf42c15a08774b062aafb089422e1b74cf5
Tu Dang <tudang87@gmail.com>
2015-10-12 17:52:19 +0200

    install openvswitch

--- setup/mininet-setup.sh
+++ setup/mininet-setup.sh
@@ -4,9 +4,10 @@
 cd ~
 sudo rm -rf mininet
 sudo rm -rf openflow
-sudo apt-get remove -y mininet
+sudo apt-get remove -y mininet openvswitch-switch openvswitch-controller
 
 # Now re-install Mininet
+sudo apt-get install -y openvswitch-switch openvswitch-controller
 sudo apt-get install -y mininet
 #git clone git://github.com/mininet/mininet
 #pushd mininet
