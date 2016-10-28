c740b1ebbe1921d3b78ea473f0355f3ae57f9eef
Robert Soulé <robert.soule@usi.ch>
2016-10-28 11:55:14 +0200

    vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -24,11 +24,6 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
  ## Provisioning
  config.vm.provision :puppet
  config.vm.provision :shell, privileged: false, :path => "setup/mininet.sh"
- #config.vm.provision :shell, privileged: false, :path => "setup/p4paxos-setup.sh"
- #config.vm.provision :shell, privileged: false, :path => "setup/p4-setup.sh"
- #config.vm.provision :shell, privileged: false, :path => "setup/p4tut-setup.sh"
- #config.vm.provision :shell, privileged: false, :path => "setup/spin-setup.sh"
- #config.vm.provision :shell, privileged: false, :path => "setup/datalog-setup.sh"
 
  ## SSH config
  config.ssh.forward_x11 = true
--- manifests/default.pp
+++ manifests/default.pp
@@ -17,7 +17,8 @@ Exec["apt-update"] -> Package <| |> #This means that an apt-update command has t
 
 Package { ensure => "installed" }
 
-$essentials  = [ "build-essential", "fakeroot", "debhelper", "autoconf", "automake", "libssl-dev", "graphviz", "python-all", "python-qt4", "python-twisted-conch", "libtool", "tmux", "vim", "python-pip", "python-paramiko", "python-sphinx", "python-dev" , "ssh", "emacs", "sshfs", "python-routes", "bison", "git", "xterm", "firefox" ]
+$essentials  = [ "build-essential", "fakeroot", "debhelper", "autoconf", "automake", "libssl-dev", "graphviz", "python-all", "python-qt4", "python-twisted-conch", "libtool", "tmux", 
+                 "vim", "python-pip", "python-paramiko", "python-sphinx", "python-dev" , "ssh", "emacs", "sshfs", "python-routes", "bison", "git", "xterm", "firefox"  ]
 
 $pipPackages = [ "alabaster", "greenlet", "networkx" , "decorator", "eventlet", "msgpack-python", "oslo.config", "scapy", "thrift" ]
 
@@ -33,33 +34,6 @@ package { "ryu":
   ensure => latest,
   provider => pip,
   require => Package["python-routes", "eventlet", "msgpack-python", "oslo.config"],
-}
-
-#package {"openvswitch-switch":
-#  ensure  => present,
-#}
-#
-#package {"openvswitch-controller":
-#  ensure  => present,
-#}
-#
-#package {"mininet":
-#  ensure  => latest,
-#  require => Package["openvswitch-switch", "openvswitch-controller"],
-#}
-
-# install swipl
-package {"software-properties-common": 
-  ensure => latest,
-}
-
-exec {"apt-add-repository ppa:swi-prolog/stable":
-    path => "/usr/bin",
-}
-
-package {"swi-prolog":
-  ensure => "installed",
-  require => Exec["apt-add-repository ppa:swi-prolog/stable"],
 }
 
 package {"wireshark":
