8538cb5901133930013e787fad02f1d338e78ab1
Robert Soulé <robert.soule@usi.ch>
2016-11-22 15:42:11 +0100

    change to git checkout

--- setup/bmv2.sh
+++ setup/bmv2.sh
@@ -4,9 +4,10 @@
 sudo apt-get install -y git autoconf python-pip build-essential python-dev \
     cmake libjudy-dev libgmp-dev libpcap-dev mktemp libffi-dev r-base gawk
 
-sudo rm -rf behavioral-model p4c-bm
-git clone https://github.com/p4lang/behavioral-model.git
-git clone https://github.com/p4lang/p4c-bm.git
+sudo rm -rf bmv2 p4c-bmv2 behavioral-model p4c-bm
+
+git clone https://github.com/p4lang/behavioral-model.git bmv2
+git clone https://github.com/p4lang/p4c-bm.git p4c-bmv2
 
 # # install thrift
 sudo apt-get install -y libboost-dev libboost-test-dev libboost-program-options-dev \
@@ -24,24 +25,24 @@ sudo make -j2
 sudo make install
 cd ..
 # install nanomsg
-bash ../behavioral-model/travis/install-nanomsg.sh
+bash ../bmv2/travis/install-nanomsg.sh
 sudo ldconfig
 
 # install nnpy
-bash ../behavioral-model/travis/install-nnpy.sh
+bash ../bmv2/travis/install-nnpy.sh
 
 # clean up
 cd ..
 sudo rm -rf $tmpdir
 
-cd behavioral-model
+cd bmv2
 ./autogen.sh
 # better performance for behavioral-model
 #./configure --disable-logging-macros --disable-elogger CXXFLAGS=-O3
 ./configure
 sudo make
 
-cd ../p4c-bm
+cd ../p4c-bmv2
 sudo pip install -r requirements.txt
 sudo pip install -r requirements_v1_1.txt
 sudo python setup.py install
